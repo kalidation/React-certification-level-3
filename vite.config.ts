ec0157c0fae2b1d467a740ed41ec89604c5bad36
khaled bougdour <kalidation@gmail.com>
2024-10-19 19:25:16 +0200

    config github

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 import * as path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
-    base: command === "serve" ? "/" : "/React-certification-level-3",
+    base: "/React-certification-level-3/",
     plugins: [react()],
     resolve: {
       alias: {
